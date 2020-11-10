0d5b292c13b9da97311d902aec8d9d6f72724fbf
unknown <tyrone.tong800@gmail.com>
2020-11-10 02:06:07 -0800

    login page

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
 
-driver = webdriver.Firefox(executable_path="C:/web_driver/geckodriver")
-driver.get('https://google.com')
+
+
+class Instagram:
+
+    def __init__ (self, username, password):
+        self.username = username
+        self.password = password
+        self.driver = webdriver.Firefox(executable_path="geckodriver")
+
+    def exitbrowser(self):
+        self.driver.close()
+
+    def login(self):
+        driver = self.driver
+        driver.get('https://www.instagram.com/')
+        time.sleep(2)
+        #"//input[@name='username']"
+        #"//input[@name='password']"
+
+
+if __name__ == "__main__":
+    # ig = Instagram("tyrone.tong", "(Tong800000)")
+    driver = webdriver.Firefox(executable_path="geckodriver")
+    driver.get('https://www.instagram.com/')
